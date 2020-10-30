c2a02f82071f61285d7110527349edf1e01280f1
hanelso <gjsxj1992@gmail.com>
2020-10-30 15:36:51 +0900

    Flask Example start
    flask 에서 sqlite3를 이용하여 ORM을 사용하는 방법을 추가하였다.

---
+++ sqlEx3.py
@@ -0,0 +1,10 @@
+﻿import sqlite3
+
+conn = sqlite3.connect("./chadwick.db")
+print("Opened database successfully")
+
+cursor = conn.execute("select yearID from Salaries limit 10")
+
+for row in cursor:
+    print(row )
+    print("yearID=", row[0] )
